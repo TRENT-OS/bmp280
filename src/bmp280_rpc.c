028860df2045bb9f9eed8f0f61bc1b3ef71b3772
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-26 14:44:34 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- src/bmp280_rpc.c
+++ src/bmp280_rpc.c
@@ -1,4 +1,10 @@
-
+/*
+ * Copyright (C) 2022-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
+ */
 
 #include "bmp280.h"
 #include <camkes.h>
